4ae4bd6f4e9861be504d0c5726cc2b042c470670
Federico Carranza <fcarranz@student.42barcelona.com>
2025-03-05 21:45:14 +0100

    ex03 - Finish AMateria class

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -6,14 +6,34 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 18:56:23 by fcarranz          #+#    #+#             */
-/*   Updated: 2025/03/05 19:43:57 by fcarranz         ###   ########.fr       */
+/*   Updated: 2025/03/05 21:41:52 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+#include <string>
 
+/*
+* Constructors
+ */
 AMateria::AMateria(void) : _type("AMateria") {}
 
 AMateria::AMateria(std::string const &type) : _type(type) {}
 
-std::string const &AMateria::getType() const { return _type; }
+AMateria::AMateria(std::string const & type) { _type = other._type; }
+
+/*
+* Destructor
+ */
+AMateria::~AMateria(void) {}
+
+/*
+* Overload operator
+ */
+AMateria &operator=(const AMateria &other)
+{
+  if (this != &other) {
+    _type = other._type;
+  }
+  return *this;
+}
--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -6,15 +6,14 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 19:04:22 by fcarranz          #+#    #+#             */
-/*   Updated: 2025/03/05 19:33:20 by fcarranz         ###   ########.fr       */
+/*   Updated: 2025/03/05 22:32:51 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <string>
 
-class AMateria
-{
+class AMateria {
 protected:
   std::string _type;
 public:
@@ -22,9 +21,9 @@ public:
   AMateria(std::string const & type);
   virtual ~AMateria(void);
 
-  AMateria &operator=(const AMateria &other);
+  AMateria &operator=(AMateria const &other);
 
-  std::string const & getType() const; //Returns the materia type
-  virtual AMateria* clone() const = 0;
-  virtual void use(ICharacter& target);
+  virtual std::string const & getType(void) const = 0; //Returns the materia type
+  virtual AMateria* clone(void) const = 0;
+  virtual void use(ICharacter& target) = 0;
 };

1a50cf7c87ec1f0b5f774a106a7a194f5d4527e2
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2025-03-09 12:52:15 +0100

    ex03 - Implement ICharacter interface

---
+++ ex03/ICharacter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcarranz <fcarranz@student.42barcelo>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 11:04:26 by fcarranz          #+#    #+#             */
+/*   Updated: 2025/03/09 12:51:37 by fcarranz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+
+class ICharacter
+{
+public:
+  virtual ~ICharacter(void) {}
+  virtual std::string const & getName() const = 0;
+  virtual void equip(AMateria* m) = 0;
+  virtual void unequip(int idx) = 0;
+  virtual void use(int idx, ICharacter& target) = 0;
+};
